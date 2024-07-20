aa0ed7a5d00d1091ee8f8f347a773af2dcb97537
ShankarSingh06 <shankar4rk@gmail.com>
2024-07-20 11:52:02 +0000

    conversion Operation

--- Basic/01_variables.js
+++
@@ -1,7 +0,0 @@
-const accountID=12345
-let accountEmail="shankar.singh@google.com"
-var accountPassword="12345"
-let accountCity="Bangalore"
-
-console.log(accountID);
-console.table[accountID,accountEmail,accountPassword,accountCity]
